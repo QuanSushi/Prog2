26be9dfb466be71e63b79a0b079f0c58a611d006
Jaqobs <me@quan-sushi.com>
2015-01-25 22:24:02 +0100

    added credits

--- AdventureGame/src/start/TestStart.java
+++ AdventureGame/src/start/TestStart.java
@@ -1,5 +1,18 @@
 package start;
 
+/**
+ * authors: Tran Anh Quan, Do Long
+ * 
+ * Gits: 
+ * https://github.com/QuanSushi/Prog2
+ * https://github.com/longdo93/Prog2
+ * 
+ * Java version: 1.7
+ * Programm Version: 1.0
+ * 
+ * known bugs: infinite quiz when using arrow keys to move to magic room.
+ */
+
 import GUI.MainFrame;
 import dungeon.Client;
 import dungeon.Factory;
